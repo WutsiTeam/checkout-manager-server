c76e69cb2be13a3305fae37c0e20b595c29beed8
wutsi <herve.tchepannou@wutsi.com>
2022-11-11 07:36:26 -0500

    remove cache

--- openapi-codegen.sh
+++ openapi-codegen.sh
@@ -18,7 +18,6 @@ java -jar ${CODEGEN_JAR} server \
     -heroku ${API_NAME}-server \
     -service_logger \
     -service_mqueue \
-    -service_cache \
     -service_slack \
     -service_swagger
 

15f175712c1a080af8a7a356c89c9a6c3db3d457
wutsi <herve.tchepannou@wutsi.com>
2022-11-29 06:31:12 -0500

    log request payload

--- src/main/kotlin/com/wutsi/checkout/manager/delegate/SearchPaymentMethodDelegate.kt
+++ src/main/kotlin/com/wutsi/checkout/manager/delegate/SearchPaymentMethodDelegate.kt
@@ -3,11 +3,20 @@ package com.wutsi.checkout.manager.`delegate`
 import com.wutsi.checkout.manager.dto.SearchPaymentMethodRequest
 import com.wutsi.checkout.manager.dto.SearchPaymentMethodResponse
 import com.wutsi.checkout.manager.workflow.SearchPaymentMethodWorkflow
+import com.wutsi.platform.core.logging.KVLogger
 import com.wutsi.workflow.WorkflowContext
 import org.springframework.stereotype.Service
 
 @Service
-public class SearchPaymentMethodDelegate(private val workflow: SearchPaymentMethodWorkflow) {
-    public fun invoke(request: SearchPaymentMethodRequest): SearchPaymentMethodResponse =
-        workflow.execute(request, WorkflowContext())
+public class SearchPaymentMethodDelegate(
+    private val logger: KVLogger,
+    private val workflow: SearchPaymentMethodWorkflow
+) {
+    public fun invoke(request: SearchPaymentMethodRequest): SearchPaymentMethodResponse {
+        logger.add("request_status", request.status)
+        logger.add("request_limit", request.limit)
+        logger.add("request_offset", request.offset)
+
+        return workflow.execute(request, WorkflowContext())
+    }
 }
--- src/main/kotlin/com/wutsi/checkout/manager/delegate/SearchPaymentProviderDelegate.kt
+++ src/main/kotlin/com/wutsi/checkout/manager/delegate/SearchPaymentProviderDelegate.kt
@@ -3,11 +3,21 @@ package com.wutsi.checkout.manager.`delegate`
 import com.wutsi.checkout.manager.dto.SearchPaymentProviderRequest
 import com.wutsi.checkout.manager.dto.SearchPaymentProviderResponse
 import com.wutsi.checkout.manager.workflow.SearchPaymentProviderWorkflow
+import com.wutsi.platform.core.logging.KVLogger
 import com.wutsi.workflow.WorkflowContext
 import org.springframework.stereotype.Service
 
 @Service
-public class SearchPaymentProviderDelegate(private val workflow: SearchPaymentProviderWorkflow) {
-    public fun invoke(request: SearchPaymentProviderRequest): SearchPaymentProviderResponse =
-        workflow.execute(request, WorkflowContext())
+public class SearchPaymentProviderDelegate(
+    private val logger: KVLogger,
+    private val workflow: SearchPaymentProviderWorkflow
+) {
+    public fun invoke(request: SearchPaymentProviderRequest): SearchPaymentProviderResponse {
+        logger.add("request_country", request.country)
+        logger.add("request_number", request.number)
+        logger.add("request_type", request.type)
+
+        return workflow.execute(request, WorkflowContext())
+    }
+
 }

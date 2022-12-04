3754e80d139a0cd437f25993c4b36eeb962e0912
wutsi <herve.tchepannou@wutsi.com>
2022-12-04 17:09:53 -0500

    send notification on sync

--- src/main/kotlin/com/wutsi/checkout/manager/delegate/GetTransactionDelegate.kt
+++ src/main/kotlin/com/wutsi/checkout/manager/delegate/GetTransactionDelegate.kt
@@ -2,17 +2,30 @@ package com.wutsi.checkout.manager.`delegate`
 
 import com.wutsi.checkout.manager.dto.GetTransactionResponse
 import com.wutsi.checkout.manager.workflow.GetTransactionWorkflow
+import com.wutsi.checkout.manager.workflow.ProcessPendingTransactionWorkflow
 import com.wutsi.workflow.WorkflowContext
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
-public class GetTransactionDelegate(private val workflow: GetTransactionWorkflow) {
-    public fun invoke(id: String, sync: Boolean?): GetTransactionResponse =
-        workflow.execute(
-            id,
-            WorkflowContext(
-                data = sync?.let { mutableMapOf(GetTransactionWorkflow.DATA_SYNC to it) }
-                    ?: mutableMapOf()
-            )
-        )
+public class GetTransactionDelegate(
+    private val workflow: GetTransactionWorkflow,
+    private val pendingWorkflow: ProcessPendingTransactionWorkflow
+) {
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(GetTransactionDelegate::class.java)
+    }
+
+    public fun invoke(id: String, sync: Boolean?): GetTransactionResponse {
+        val context = WorkflowContext()
+
+        if (sync == true) {
+            try {
+                pendingWorkflow.execute(id, context)
+            } catch (ex: Exception) {
+                LOGGER.warn("Unable to sync the transaction", ex)
+            }
+        }
+        return workflow.execute(id, context)
+    }
 }
--- src/main/kotlin/com/wutsi/checkout/manager/workflow/GetTransactionWorkflow.kt
+++ src/main/kotlin/com/wutsi/checkout/manager/workflow/GetTransactionWorkflow.kt
@@ -3,7 +3,6 @@ package com.wutsi.checkout.manager.workflow
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.wutsi.checkout.manager.dto.GetTransactionResponse
 import com.wutsi.checkout.manager.dto.Transaction
-import com.wutsi.platform.core.logging.KVLogger
 import com.wutsi.platform.core.stream.EventStream
 import com.wutsi.workflow.RuleSet
 import com.wutsi.workflow.WorkflowContext
@@ -12,13 +11,8 @@ import org.springframework.stereotype.Service
 @Service
 class GetTransactionWorkflow(
     private val objectMapper: ObjectMapper,
-    private val logger: KVLogger,
     eventStream: EventStream
 ) : AbstractCheckoutWorkflow<String, GetTransactionResponse, Void>(eventStream) {
-    companion object {
-        const val DATA_SYNC = "sync"
-    }
-
     override fun getEventType(): String? = null
 
     override fun toEventPayload(
@@ -30,8 +24,6 @@ class GetTransactionWorkflow(
     override fun getValidationRules(transactionId: String, context: WorkflowContext) = RuleSet.NONE
 
     override fun doExecute(transactionId: String, context: WorkflowContext): GetTransactionResponse {
-        sync(transactionId, context)
-
         val transaction = checkoutAccessApi.getTransaction(transactionId).transaction
         /*
          * Because the order data structure is the same as the one from access layer, let use json serialization for copying the data
@@ -40,20 +32,5 @@ class GetTransactionWorkflow(
         return GetTransactionResponse(
             transaction = objectMapper.readValue(json, Transaction::class.java)
         )
-    }
-
-    private fun sync(transactionId: String, context: WorkflowContext) {
-        if (context.data[DATA_SYNC] != true) {
-            return
-        }
-
-        try {
-            checkoutAccessApi.syncTransactionStatus(transactionId)
-            logger.add("sync_success", true)
-        } catch (ex: Exception) {
-            logger.add("sync_success", false)
-            logger.add("sync_exception", ex::javaClass.name)
-            logger.add("sync_error", ex.message)
-        }
     }
 }

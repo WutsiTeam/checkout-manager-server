aa18a2452618e541957f8b5782692e5dc28fc9d6
wutsi <herve.tchepannou@wutsi.com>
2023-03-10 18:37:08 -0500

    update event handling

--- src/main/kotlin/com/wutsi/checkout/manager/event/EventHandler.kt
+++ src/main/kotlin/com/wutsi/checkout/manager/event/EventHandler.kt
@@ -1,22 +1,17 @@
 package com.wutsi.checkout.manager.event
 
-import com.wutsi.event.EventURN
 import com.wutsi.platform.core.stream.Event
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Service
 
 @Service
 class EventHandler(
-    private val membership: MembershipEventHandler,
     private val transaction: TransactionEventHandler,
     private val order: OrderEventHandler,
 ) {
     @EventListener
     fun handleEvent(event: Event) {
         when (event.type) {
-            EventURN.MEMBER_REGISTERED.urn -> membership.onMemberRegistered(event)
-            EventURN.MEMBER_DELETED.urn -> membership.onMemberDeleted(event)
-
             InternalEventURN.TRANSACTION_SUCCESSFUL.urn -> transaction.onTransactionSuccessful(event)
             InternalEventURN.ORDER_TO_CUSTOMER_SUBMITTED.urn -> order.onSendToCustomer(event)
             InternalEventURN.ORDER_TO_MERCHANT_SUBMITTED.urn -> order.onSendToMerchant(event)
